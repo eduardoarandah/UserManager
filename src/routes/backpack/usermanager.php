0b3deb6544a82a325d3525c525223ec33e276d5b
lalo <eduardoarandah@gmail.com>
2018-05-30 06:10:35 -0500

    ups, wrong routes

--- src/routes/backpack/usermanager.php
+++ src/routes/backpack/usermanager.php
@@ -2,20 +2,18 @@
 
 /*
 |--------------------------------------------------------------------------
-| Backpack\PermissionManager Routes
+| Backpack\UserManager Routes
 |--------------------------------------------------------------------------
 |
 | This file is where you may define all of the routes that are
-| handled by the Backpack\PermissionManager package.
+| handled by the Backpack\UserManager package.
 |
 */
 
 Route::group([
-            'namespace'  => 'Backpack\PermissionManager\app\Http\Controllers',
+            'namespace'  => 'Backpack\UserManager\app\Http\Controllers',
             'prefix'     => config('backpack.base.route_prefix', 'admin'),
             'middleware' => ['web', backpack_middleware()],
     ], function () {
-        CRUD::resource('permission', 'PermissionCrudController');
-        CRUD::resource('role', 'RoleCrudController');
         CRUD::resource('user', 'UserCrudController');
     });
